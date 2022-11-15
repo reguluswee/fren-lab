79bbc9fabfcc10e3929fdd39ee6e88609f9fde73
regulus.wee <regulus.wee@gmail.com>
2022-11-15 17:50:48 +0800

    update swap contract

--- migrations/2_deploy_Treasury.js
+++ migrations/2_deploy_Treasury.js
@@ -1,4 +1,5 @@
-const Treasury = artifacts.require("TreasuryOne");
+// const Treasury = artifacts.require("TreasuryOne");
+const Treasury = artifacts.require("TreasuryTwo");
 
 module.exports = async function (deployer) {
   await deployer.deploy(Treasury);
