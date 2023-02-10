aa42dd3df5f849c94ba3401bc1149f95735ae2db
regulus.wee <regulus.wee@gmail.com>
2023-02-10 15:33:08 +0800

    treasury script

--- migrations/2_deploy_Treasury.js
+++ migrations/2_deploy_Treasury.js
@@ -1,5 +1,7 @@
 // const Treasury = artifacts.require("TreasuryOne");
-const Treasury = artifacts.require("TreasuryThree");
+//const Treasury = artifacts.require("TreasuryThree"); 0xcCa5db687393a018d744658524B6C14dC251015f
+
+const Treasury = artifacts.require("TreasuryFour");
 
 module.exports = async function (deployer) {
   await deployer.deploy(Treasury);
