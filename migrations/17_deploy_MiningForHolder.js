c9e8be89fa99c9b39761bdc0ec31d16a419efbf8
regulus.wee <regulus.wee@gmail.com>
2023-04-18 23:53:16 +0800

    1

--- migrations/17_deploy_MiningForHolder.js
+++ migrations/17_deploy_MiningForHolder.js
@@ -1,8 +1,11 @@
 const MiningForHolder = artifacts.require("MiningForHolder");
 
+// const root = "0x000000000000000000000000000000000000000000000000000000000000000"
+const root = "0xcda14a162a64a888a53950f01a6485d8dc30ad86bfde16d27fd3d9399695fa2c"
+
 module.exports = async function (deployer) {
 
-  await deployer.deploy(MiningForHolder, "0x000000000000000000000000000000000000000000000000000000000000000");
+  await deployer.deploy(MiningForHolder, root);
   console.log('New MiningForHolder Contract deploy succeed:', MiningForHolder.address)
 
 };
