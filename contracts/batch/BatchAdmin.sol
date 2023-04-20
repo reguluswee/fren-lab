9fd9dc91e2a9d1146dd96dab06c0183144721696
regulus.wee <regulus.wee@gmail.com>
2023-04-20 17:41:21 +0800

    no use

--- contracts/batch/BatchAdmin.sol
+++ contracts/batch/BatchAdmin.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-//import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import "./BatchProxy.sol";
 
